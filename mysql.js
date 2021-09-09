cf6d55852f1550fa81667c34f1c6e9dfd42b28e9
nvdinh09thxd <nvdinh09thxd@gmail.com>
2021-09-09 16:16:53 +0700

    x

---
+++ mysql.js
@@ -0,0 +1,33 @@
+const knex = require('knex')({
+  client: 'mysql',
+  connection: {
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'mydb'
+  },
+  useNullAsDefault: true
+});
+
+(async () => {
+  try {
+    // Create a table
+    await knex.schema
+      .createTable('users1', table => {
+        table.increments('id');
+        table.string('username');
+      });
+
+    // Then query the table...
+    await knex('users1').insert({ username: 'Tim' });
+
+    // Query both of the rows.
+    const selectedRows = await knex('users1').select('username');
+
+    // map over the results
+    const enrichedRows = selectedRows.map(row => ({ ...row }));
+    console.log(enrichedRows);
+  } catch (e) {
+    console.error(e);
+  }
+})();
---
+++ sqlite3.js
@@ -0,0 +1,30 @@
+const knex = require('knex')({
+  client: 'sqlite3',
+  connection: {
+    filename: './mydb.db',
+  },
+  useNullAsDefault: true
+});
+
+(async () => {
+  try {
+    // Create a table
+    await knex.schema
+      .createTable('users1', table => {
+        table.increments('id');
+        table.string('username');
+      });
+
+    // Then query the table...
+    await knex('users1').insert({ username: 'Tim' });
+
+    // Query both of the rows.
+    const selectedRows = await knex('users1').select('username');
+
+    // map over the results
+    const enrichedRows = selectedRows.map(row => ({ ...row }));
+    console.log(enrichedRows);
+  } catch (e) {
+    console.error(e);
+  }
+})();
