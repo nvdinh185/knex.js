6ea2875d964d8d3a2bc15a592a2569a154ac0883
nvdinh09thxd <nvdinh09thxd@gmail.com>
2021-09-14 11:34:19 +0700

    mysql crud

---
+++ mysql_2.js
@@ -0,0 +1,37 @@
+const knex = require('knex')({
+  client: 'mysql',
+  connection: {
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'mydb'
+  },
+  useNullAsDefault: true
+});
+
+(async () => {
+  try {
+    // Create a table
+    await knex.schema
+      .createTable('JOB_TYPE', table => {
+        table.increments('JOB_ID');
+        table.string('JOB_NAME');
+      });
+
+    await knex('JOB_TYPE').insert({ JOB_ID: 1, JOB_NAME: "NEW job 111" });
+    await knex('JOB_TYPE').insert({ JOB_NAME: "NEW job 222" });
+
+    await knex('JOB_TYPE').where('JOB_ID', '=', 1).update({ JOB_NAME: '111' });
+
+    // await knex('JOB_TYPE').where('JOB_ID', '=', 12).del();
+
+    const selectedRows = await knex("JOB_TYPE").select();
+    // const selectedRows = await knex("JOB_TYPE").select().where('JOB_ID', '11');
+
+    // map over the results
+    const enrichedRows = selectedRows.map(row => ({ ...row }));
+    console.log(enrichedRows);
+  } catch (e) {
+    console.error(e);
+  }
+})();
